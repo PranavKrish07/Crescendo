acddfd7e3554bf84dbe7edba3c8067e1c0a1ecd7
Pranav Krishna <pranavkrishna404@gmail.com>
2026-06-12 18:16:25 +0530

    build sh

---
+++ backend/build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# Install dependencies
+pip install -r requirements.txt
+
+python manage.py collectstatic --no-input
+
+python manage.py migrate
+
+python seed_roadmaps.py

8ef4f075ff8739a8a120f22f6190658bc3c58ae2
Pranav Krishna <pranavkrishna404@gmail.com>
2026-06-12 20:52:00 +0530

    build.sh

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -68,10 +68,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "https://crescendo-tau-five.vercel.app",
-]
+CORS_ALLOW_ALL_ORIGINS = True
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',

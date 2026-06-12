500aec946103957ed4fac3115df3c47e48bb17e3
Pranav Krishna <pranavkrishna404@gmail.com>
2026-06-12 20:24:59 +0530

    build.sh

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -124,7 +124,8 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600
+        conn_max_age=600,
+        engine='django.db.backends.postgresql'
     )
 }
 

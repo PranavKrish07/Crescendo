6a1a928d5d99523c6ce1aa803fce180eb0780d01
Pranav Krishna <pranavkrishna404@gmail.com>
2026-06-13 09:57:17 +0530

    slash

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://crescendo-xmd9.onrender.com/api/auth",
+  baseURL: "https://crescendo-xmd9.onrender.com/api/auth/",
   headers: {
     "Content-Type": "application/json",
   },

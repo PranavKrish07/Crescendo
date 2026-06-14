e197189e51676d896e5d8cbce71183bad613656e
Pranav Krishna <pranavkrishna404@gmail.com>
2026-06-14 11:23:16 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://crescendo-xmd9.onrender.com/api/auth/",
+  baseURL: `${import.meta.env.VITE_API_BASE_URL}/api/auth/`,
   headers: {
     "Content-Type": "application/json",
   },

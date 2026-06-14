f109b9c3f88aa5ea98571fc4c8124e0d04609882
Pranav Krishna <pranavkrishna404@gmail.com>
2026-06-14 09:28:11 +0530

    dashboard

--- frontend/src/api/dashboard.js
+++ frontend/src/api/dashboard.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const dashboardAPI = axios.create({
-  baseURL: "http://localhost:8000/api/dashboard",
+  baseURL: "https://crescendo-xmd9.onrender.com/api/dashboard",
   headers: {
     "Content-Type": "application/json",
   },
